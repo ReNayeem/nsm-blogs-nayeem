625f377fca656d1bea498b3ba4dc52e5279c6047
Md. Nayeem <renayeem@gmail.com>
2023-09-10 21:13:52 +0600

    dark mode failed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import ThemeToggle from '@/components/global/ThemeToggle';
+import './../css/page.css';
 
 export default function Home() {
   return (
-    <div className='flex items-center gap-2'>
-      Click the button to change the theme
-      <ThemeToggle />
+    <div>
+      <div className='flex items-center gap-2'>
+        Click the button to change the theme
+        <ThemeToggle />
+      </div>
+
+      {/* <button className="border-solid border-2 border-black pb-1 px-2 rounded hover:bg-black hover:text-white ease-in-out duration-300">Save Changes</button> */}
+      <button className="btn-testing">Save Changes</button>
     </div>
   );
 }
