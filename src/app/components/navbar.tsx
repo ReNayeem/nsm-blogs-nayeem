33182a9e6e932bb26d2a105c98702d6a766c28eb
Md. Nayeem <renayeem@gmail.com>
2023-09-11 21:25:37 +0600

    nav responsiveness done

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { Dropdown, Navbar } from 'flowbite-react';
+import './../../css/navbar.css'
 
 const navbar = () => {
     return (
@@ -41,15 +42,16 @@ const navbar = () => {
                     <Navbar.Link href="/navbars">
                         Contact
                     </Navbar.Link>
-                    <div className="relative">
+
+                    <div className="relative mt-1 search-bar">
                         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
-                            <span className="sr-only">Search icon</span>
                         </div>
                         <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
                     </div>
+
                 </Navbar.Collapse>
             </Navbar>
 
