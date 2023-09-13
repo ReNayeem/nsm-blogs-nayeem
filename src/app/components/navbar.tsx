16a23b4f747b06b9cc7666126d3179f86742c613
Md. Nayeem <renayeem@gmail.com>
2023-09-13 22:17:28 +0600

    Navbar completed except!
    - dark mode/light mode done
    - responsiveness done
    - design for now mostly done
    - if I use Link tag for route shows 3 errors including hydration error

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { Dropdown, Navbar } from 'flowbite-react';
 import './../../css/navbar.css'
+import { ModeToggle } from '@/components/global/ModeToggle';
 
 const navbar = () => {
     return (
@@ -11,38 +12,59 @@ const navbar = () => {
             <Navbar
                 fluid={true}
                 rounded={true}
-                className='bg-transparent'
+                className='bg-transparent dark:bg-transparent'
             >
-                <Navbar.Brand href="https://flowbite.com/">
+                <Navbar.Brand>
                     <img
                         src="https://flowbite.com/docs/images/logo.svg"
-                        className="mr-3 h-14 w-14"
+                        className="mr-3 lg:h-14 lg:w-14 md:h-10 md:w-10"
                         alt="Flowbite Logo"
                     />
-                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                    <span className="self-center whitespace-nowrap text-2xl md:text-xl font-semibold dark:text-white">
                         NSM
                     </span>
                 </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse>
+
                     <Navbar.Link
-                        href="/navbars"
+                        href="/"
                         // active={true}
-                        className='text-lg leading-none font-normal tracking-wide text-zinc-950 dark:text-slate-300'
+                        className='lg:text-lg md:text-sm leading-none font-normal tracking-wide text-zinc-950 dark:text-slate-300'
                     >
                         Home
                     </Navbar.Link>
-                    <Navbar.Link href="/navbars">
-                        About
+
+                    <Navbar.Link
+                        href="/"
+                        // active={true}
+                        className='lg:text-lg md:text-sm leading-none font-normal tracking-wide text-zinc-950 dark:text-slate-300'
+                    >
+                        Games
                     </Navbar.Link>
-                    <Navbar.Link href="/navbars">
-                        Services
+
+                    <Navbar.Link
+                        href="/"
+                        // active={true}
+                        className='lg:text-lg md:text-sm leading-none font-normal tracking-wide text-zinc-950 dark:text-slate-300'
+                    >
+                        Anime
                     </Navbar.Link>
-                    <Navbar.Link href="/navbars">
-                        Pricing
+
+                    <Navbar.Link
+                        href="/"
+                        // active={true}
+                        className='lg:text-lg md:text-sm leading-none font-normal tracking-wide text-zinc-950 dark:text-slate-300'
+                    >
+                        Reviews
                     </Navbar.Link>
-                    <Navbar.Link href="/navbars">
-                        Contact
+
+                    <Navbar.Link
+                        href="/"
+                        // active={true}
+                        className='lg:text-lg md:text-sm leading-none font-normal tracking-wide text-zinc-950 dark:text-slate-300'
+                    >
+                        News
                     </Navbar.Link>
 
                     <div className="relative mt-1 search-bar">
@@ -51,8 +73,9 @@ const navbar = () => {
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
-                        <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
+                        <input type="text" id="search-navbar" className="block w-full p-2 pl-10 lg:text-lg md:text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
                     </div>
+                    <ModeToggle />
 
                 </Navbar.Collapse>
             </Navbar>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,6 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div>
-      <div className='flex items-center gap-2'>
-        Click the button to change the theme
-        <ModeToggle />
-      </div>
-
-      {/* Example of using ShadcnUI */}
-      <Button variant='destructive'>Click me</Button>
-      <button className="border-solid border-2 border-black pb-1 px-2 rounded hover:bg-black hover:text-white ease-in-out duration-300 hover:dark:text-black hover:dark:bg-white dark:border-white">Save Changes</button>
     </div>
   );
 }
